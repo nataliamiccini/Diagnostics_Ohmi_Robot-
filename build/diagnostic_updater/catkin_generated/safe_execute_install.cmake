1d1f5e3756c3031695166bce753445255034f0e0
Natalia Miccini <55144535+nataliamiccini@users.noreply.github.com>
2022-04-27 12:52:15 +0200

    Add files via upload

---
+++ build/diagnostic_updater/catkin_generated/safe_execute_install.cmake
@@ -0,0 +1,5 @@
+execute_process(COMMAND "/home/natalia/Scrivania/tb-simulation/ros_ws/build/diagnostic_updater/catkin_generated/python_distutils_install.sh" RESULT_VARIABLE res)
+
+if(NOT res EQUAL 0)
+  message(FATAL_ERROR "execute_process(/home/natalia/Scrivania/tb-simulation/ros_ws/build/diagnostic_updater/catkin_generated/python_distutils_install.sh) returned error code ")
+endif()
