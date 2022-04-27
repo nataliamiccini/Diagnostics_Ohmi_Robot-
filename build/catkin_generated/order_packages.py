5ee3b757075b78b484b48683c78e41d195cadcc0
Natalia Miccini <55144535+nataliamiccini@users.noreply.github.com>
2022-04-27 12:48:47 +0200

    Add files via upload

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/natalia/Scrivania/tb-simulation/ros_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/natalia/Scrivania/tb-simulation/ros_ws/devel;/opt/ros/melodic'.split(';') if '/home/natalia/Scrivania/tb-simulation/ros_ws/devel;/opt/ros/melodic' != '' else []
