d922398a0b587af579af6eb854dab1d761f6d7be
Natalia Miccini <55144535+nataliamiccini@users.noreply.github.com>
2022-04-29 18:05:07 +0200

    Delete build directory

--- build/diagnostic_common_diagnostics/catkin_generated/safe_execute_install.cmake
+++
@@ -1,5 +0,0 @@
-execute_process(COMMAND "/home/natalia/Scrivania/tb-simulation/ros_ws/build/diagnostic_common_diagnostics/catkin_generated/python_distutils_install.sh" RESULT_VARIABLE res)
-
-if(NOT res EQUAL 0)
-  message(FATAL_ERROR "execute_process(/home/natalia/Scrivania/tb-simulation/ros_ws/build/diagnostic_common_diagnostics/catkin_generated/python_distutils_install.sh) returned error code ")
-endif()
--- build/diagnostics/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/natalia/Scrivania/tb-simulation/ros_ws/src/diagnostics
-# Build directory: /home/natalia/Scrivania/tb-simulation/ros_ws/build/diagnostics
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
