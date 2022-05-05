de11df557e7caf8cb55346e8208d1a1a44a7bdd3
Martina Rossi <49305647+martinareds@users.noreply.github.com>
2022-05-05 15:41:05 +0200

    Update main_scan.cpp
    add comments

--- ros_ws/src/tb_sim/diagnostic_updater/src/main_scan.cpp
+++ ros_ws/src/tb_sim/diagnostic_updater/src/main_scan.cpp
@@ -16,18 +16,24 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "diagnostic_updater_scan");
+  ros::init(argc, argv, "diagnostic_updater_scan"); //Ros initialization function.
   
-  ros::NodeHandle nh;
-  nh.getParam("param_scan", param_scan);
-  diagnostic_updater::Updater updaterScan;
+  ros::NodeHandle nh; //Construct a NodeHandle Class. This class is used for writing nodes
+  
+  nh.getParam("param_scan", param_scan);  //Get parameters from .yaml file and store them into indicated variables.
+  diagnostic_updater::Updater updaterScan; //Construct an updater class.
 
   updaterScan.setHardwareID("/scan");
+  
+  /*Manages a subscription callback on /scan topic 
+  and assigns a maximum size to the queue*/
   ros::Subscriber subScan = nh.subscribe("/scan", 1000, callback4);
+
   diagnostic_updater::FunctionDiagnosticTask scan("Chek scan",
        boost::bind(&check_scan, boost::placeholders::_1));
   diagnostic_updater::FunctionDiagnosticTask scan_on_off("Chek scan on/off",
        boost::bind(&check_scan_on_off, boost::placeholders::_1));
+  //Add the Diagnostic Task to our updater
   updaterScan.add(scan);
   updaterScan.add(scan_on_off);
 
@@ -39,9 +45,9 @@ int main(int argc, char **argv)
   {
     
     ros::Duration(0.1).sleep();
-
+    //spinOnce() will call all the callbacks waiting to be called at that point in time.
     ros::spinOnce();
-
+    //Call updater
     updaterScan.update();
   }
 
